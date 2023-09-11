825a68fbc412a740810679be1c9eb1d7177d5b81
Aluon <paulacanoroman@gmail.com>
2023-09-11 21:16:56 +0200

    nueva estructura

---
+++ captura-pokemon/components/Botones.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Botones = () => {
+
+
+    //INPUT 
+
+    //BOTON BUSCAR
+
+    //MODAL 
+
+    
+  return (
+    <div>Botones</div>
+  )
+}
+
+export default Botones
--- captura-pokemon/pages/screen/ScreenPokemon.jsx
+++ captura-pokemon/pages/screen/ScreenPokemon.jsx
@@ -1,25 +1,16 @@
-import InputPokemon from '@/components/InputPokemon'
+
+import Botones from '@/components/Botones'
+import DatosPokemon from '@/components/DatosPokemon'
 import React, {useState, useEffect} from 'react'
 
 const ScreenPokemon = () => {
 
-    const [datosAPI, setDatosAPI] = useState([])
-
-    useEffect(() => {
-      llamadaAPI()
-    }, [])
-
-    const llamadaAPI = async () =>  {
-        const llamada = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto`)
-        const datos = await llamada.json()
-        // console.log(datos)
-        setDatosAPI(datos)
-    }
-    
+  
   return (
     <>
     <div>ScreenPokemon</div>
-    <InputPokemon></InputPokemon>
+    <Botones></Botones>
+    <DatosPokemon></DatosPokemon>
     </>
     
   )
